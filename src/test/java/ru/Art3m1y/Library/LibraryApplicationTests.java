7d5d37cbc9af823c2cd780613ac44aae8f528625
Artem Kuryshkin <artem.kuryshkin@gmail.com>
2022-11-26 22:21:08 +0300

    Initialized spring boot application

---
+++ src/test/java/ru/Art3m1y/Library/LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Art3m1y.Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
